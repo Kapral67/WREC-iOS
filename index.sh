65776fe54b973d6c82f13763eab0a7443530a0de
Maxwell Kapral <git@maxkapral.com>
2023-10-15 00:26:40 -0700

    don't solve non-existent problems

--- index.sh
+++ index.sh
@@ -7,22 +7,16 @@
 
 . "$HOME"/Documents/secret
 
-user="$(echo "$username" | sed 's/\$/\\$/g; s/"/\\"/g')"
-pass="$(echo "$password" | sed 's/\$/\\$/g; s/"/\\"/g')"
-
-unset username
-unset password
-
 mkdir -p "$HOME/tmp" 2>/dev/null
 cookiejar="$HOME/tmp/cookiejar"
 curl -fsIL -c "$cookiejar" "https://innosoftfusiongo.com/sso/login/login-start.php?id=21" -o /dev/null
 process_endpoint="https://innosoftfusiongo.com/sso/login/login-process-fusion.php"
 curl -fsL -X POST -c "$cookiejar" -b "$cookiejar" -o /dev/null $process_endpoint -H 'Content-Type: application/x-www-form-urlencoded' \
-    --data-urlencode "username=$user" \
-    --data-urlencode "password=$pass" \
+    --data-urlencode "username=$username" \
+    --data-urlencode "password=$password" \
     --data-urlencode 'Submit=Login'
-unset user
-unset pass
+unset username
+unset password
 finish_endpoint="https://innosoftfusiongo.com/sso/login/login-finish.php"
 token="$(curl -fsiIL -c "$cookiejar" -b "$cookiejar" $finish_endpoint | awk '/Fusion-Token/ {print $2}' 2>/dev/null)"
 barcode_endpoint="https://innosoftfusiongo.com/sso/api/barcode.php?id=21"
